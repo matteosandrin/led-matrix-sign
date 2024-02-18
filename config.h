10fdd6550d8d68e3492db7f6cc463bebef1903dd
Matteo Sandrin <matteo.sandrin@hotmail.it>
2024-02-17 20:46:12 -0500

    Remove config.h file

--- config.h
+++
@@ -1,9 +0,0 @@
-
-// Get a free API key at https://api-v3.mbta.com/ and uncomment the line below
-// to add a key for the MBTA API.
-
-// #define MBTA_API_KEY "00000000000000000000000000000000"
-
-#ifndef MBTA_API_KEY
-#error "MBTA_API_KEY not defined. Please add a key for the MBTA API"
-#endif
